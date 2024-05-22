c895071c3d23bc73d82e5aefdaa8856d6075c751
Jamily Alves <149894875+Milly56@users.noreply.github.com>
2024-05-21 23:56:36 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,6 +1,21 @@
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
  
 public class Principal {
+
+String fileName = "dado.txt";
+         
+         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+             String line;
+             while ((line = br.readLine()) != null) {
+                 System.out.println(line);
+             }
+         } catch (IOException e) {
+             System.err.println("Erro ao ler o arquivo: " + e.getMessage());
+         }
+ 
     public static void main(String[] args) {
         Scanner leitorNumero = new Scanner(System.in);
  
