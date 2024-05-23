60ce0649f80f938fa649d856e71917942442d05b
Jamily Alves <149894875+Milly56@users.noreply.github.com>
2024-05-22 22:04:36 -0300

    Update Noarvore.java

--- Noarvore.java
+++ Noarvore.java
@@ -9,35 +9,52 @@ public class Noarvore {
         this.esquerda = null;
        
     }
-    
-    //verifica se a arvore esta vazia
-    public boolean estaVazia() {
-        return aluno == null;
-    }
  
+    public void imprimiPreOrdem() {
+        imprimiPreOrdem(this);
+    }
+
+    private void imprimiPreOrdem(Noarvore n) {    	
+        if (n != null) {
+            System.out.println(n.aluno);
+            imprimiPreOrdem(n.esquerda);
+            imprimiPreOrdem(n.direita);
+        }
+    }
+
+    public void imprimiPosOrdem() {
+        imprimiPosOrdem(this);
+    }
+
+    private void imprimiPosOrdem(Noarvore n) {    	
+        if (n != null) {
+            imprimiPosOrdem(n.esquerda);
+            imprimiPosOrdem(n.direita);
+            System.out.println(n.aluno);
+        }
+    }
+
+    
     public void imprimiEmOrdem() {
         imprimiEmOrdem(this);
     }
-    
-   public static void exibirGraficamente(Noarvore tree) {
-        exibirGraficamenteRecursivo(tree, "", true);
-    }
-
-    static void exibirGraficamenteRecursivo(Noarvore tree, String prefix, boolean isLeft) {
-        if (tree != null) {
-            System.out.println(prefix + (isLeft ? "├── " : "└── ") + tree.aluno);
-            exibirGraficamenteRecursivo(tree.esquerda, prefix + (isLeft ? "│   " : "    "), true);
-            exibirGraficamenteRecursivo(tree.direita, prefix + (isLeft ? "│   " : "    "), false);
-        }
-    }
-    
-    
  
-    private void imprimiEmOrdem(Noarvore n) {
-        if (n != null) {
+    private void imprimiEmOrdem(Noarvore n) {    	
+    	if (n != null) {
             imprimiEmOrdem(n.esquerda);
             System.out.println(n.aluno);
             imprimiEmOrdem(n.direita);
+    	}
+    }
+    public static void exibirGraficamente(Noarvore tree) {
+        exibirGraficamenteRecursivo(tree, "", true);
+    }
+    
+    public static void exibirGraficamenteRecursivo(Noarvore tree, String prefix, boolean isLeft) {
+        if (tree != null) {
+            System.out.println(prefix + (isLeft ? "\n├── " : "└── ") + tree.aluno);
+            exibirGraficamenteRecursivo(tree.esquerda, prefix + (isLeft ? "│   " : "    "), true);
+            exibirGraficamenteRecursivo(tree.direita, prefix + (isLeft ? "│   " : "    "), false);
         }
     }
  
@@ -47,39 +64,46 @@ public class Noarvore {
     }
  
     private void inserir(Noarvore noarvore, Aluno novoAluno) {
-        if (novoAluno.getRgm().compareTo(noarvore.aluno.getRgm()) < 0) {
-            if (noarvore.esquerda == null) {
-                noarvore.esquerda = new Noarvore(novoAluno);
-            } else {
-                inserir(noarvore.esquerda, novoAluno);
-            }
-        } else if (novoAluno.getRgm().compareTo(noarvore.aluno.getRgm()) > 0) {
-            if (noarvore.direita == null) {
-                noarvore.direita = new Noarvore(novoAluno);
-            } else {
-                inserir(noarvore.direita, novoAluno);
-            }
-        }
+    	try {
+	        if (novoAluno.getRgm().compareTo(noarvore.aluno.getRgm()) < 0) {
+	            if (noarvore.esquerda == null) {
+	                noarvore.esquerda = new Noarvore(novoAluno);
+	            } else {
+	                inserir(noarvore.esquerda, novoAluno);
+	            }
+	        } else if (novoAluno.getRgm().compareTo(noarvore.aluno.getRgm()) > 0) {
+	            if (noarvore.direita == null) {
+	                noarvore.direita = new Noarvore(novoAluno);
+	            } else {
+	                inserir(noarvore.direita, novoAluno);
+	            }
+	        }
+    	} catch(NullPointerException ponteiroNulo) {
+    		System.out.println("\nErro na tentativa em inserir um objeto vazio");
+    	}
     }
     
     public void remover(String rgm) {
         remover(this, rgm);
     }
-
+    
     private Noarvore remover(Noarvore raiz, String rgm) {
         if (raiz == null) {
             return null;
         }
 
         // Verifica se o RG do aluno na raiz é igual ao RG buscado
+        
         if (raiz.aluno.getRgm().equals(rgm)) {
             // Caso o nó tenha apenas um filho ou nenhum filho
+        	
             if (raiz.esquerda == null) {
                 return raiz.direita;
             } else if (raiz.direita == null) {
                 return raiz.esquerda;
             }
-            // Caso o nó tenha dois filhos, encontra o menor nó na subárvore direita
+         // Caso o nó tenha dois filhos, encontra o menor nó na subárvore direita
+            
             Noarvore menorNoDireita = encontrarMenorNoarvore(raiz.direita);
             raiz.aluno = menorNoDireita.aluno;
             raiz.direita = remover(raiz.direita, menorNoDireita.aluno.getRgm());
@@ -87,14 +111,16 @@ public class Noarvore {
         }
 
         // Se o RG buscado for menor que o RG do nó atual, remove da subárvore esquerda
+        
         if (rgm.compareTo(raiz.aluno.getRgm()) < 0) {
             raiz.esquerda = remover(raiz.esquerda, rgm);
+            
         } else { // Senão, remove da subárvore direita
             raiz.direita = remover(raiz.direita, rgm);
         }
         return raiz;
     }
-
+    
     private Noarvore encontrarMenorNoarvore(Noarvore no) {
         Noarvore atual = no;
         while (atual.esquerda != null) {
@@ -102,7 +128,6 @@ public class Noarvore {
         }
         return atual;
     }
-
 
     //metodo que recebe qual aluno deseja buscar pelo rgm
     public Noarvore buscar(String rgm) {
@@ -126,30 +151,28 @@ public class Noarvore {
         }
     }
     
-    
     public static void esvaziar(Noarvore raiz) {
     	if (raiz == null) {
-    		return;
     	} else {
-    	esvaziar(raiz.esquerda);
-    	esvaziar(raiz.direita);
-    	
-    	
-    	raiz.aluno = null;
-    	raiz.esquerda = null;
-    	raiz.direita = null;
-    }
-    }
-    public  static void arvoreEsvaziada(Noarvore raiz) {
-  	  if(raiz.aluno == null) {
-  		  System.out.println("Árvore esvaziada");
-  	  } else {
-  		  System.out.println("Árvore não esvaziada");
-  	  }
-    }
+    		esvaziar(raiz.esquerda);
+        	esvaziar(raiz.direita);
+        	
+        	raiz.aluno = null;
+        	raiz.esquerda = null;
+        	raiz.direita = null;
+    	}
+   }
+  
+  	public void arvoreEsvaziada(Noarvore raiz) {
+	  if(raiz.aluno == null) {
+		  System.out.println("Árvore esvaziada");
+	  } else {
+		  System.out.println("Árvore não esvaziada");
+	  }
+  }
 
-    	 @Override
-    	    public String toString() {
-    	        return aluno.toString();
-    	    }
+    @Override
+    public String toString() {
+    	return aluno.toString();
     }
+}
